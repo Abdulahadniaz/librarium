3315a3aacf161230af9700d7555abcba56e6d607
Abdul Ahad Niaz <abdulahad1028@gmail.com>
2022-01-22 01:34:13 +0500

    add title filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import api from "./fetch";
+import { api, useDebounce, useLocalStorage } from "./util";
 import Book from "./book";
 
 const url = "https://61ea80297bc0550017bc67b8.mockapi.io/api/v1/books";
 
 function App() {
   const [books, setBooks] = useState<Book[]>([]);
+  const [titleFilter, setTitleFilter] = useLocalStorage("title", "");
+  const debouncedTitle = useDebounce(titleFilter, 500);
+
+  const fetchBooks = async (url: string) => {
+    try {
+      const data = await api<Array<Book>>(url);
+      setBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    async function fetchBooks(url: string) {
-      try {
-        const data = await api<Array<Book>>(url);
-        setBooks(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchBooks(url);
-  }, []);
-  
+    const newUrl = new URL(url);
+    newUrl.searchParams.set("title", titleFilter);
+    console.log(titleFilter)
+    fetchBooks(newUrl.href);
+  }, [debouncedTitle]);
+
   return (
     <div>
+      <input
+        type="text"
+        value={titleFilter}
+        onChange={(e) => setTitleFilter(e.target.value)}
+      />
       <ul>
         {books.map((book) => {
           return <li key={book.id}>{book.title}</li>;
---
+++ src/util.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export function api<T>(url: string): Promise<T> {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    })
+    .then((data) => {
+      return data;
+    });
+}
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue] as const;
+}

1abb5244bbc3842bae168ce75620bb560eb96c2f
Abdul Ahad Niaz <abdulahad1028@gmail.com>
2022-01-23 07:53:07 +0500

    some progress

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { useLocalStorage } from "../util";
 
 interface BookItemProps {
   book: Book;
 }
 
 function BookItem({ book }: BookItemProps) {
+  const [cart, setCart] = useLocalStorage<Array<Book>>("cart", []);
+
+  function addToCart() {
+    const found = cart.filter((b: Book) => b.id === book.id);
+    console.log(found);
+    if (found.length === 0) {
+      const b = { ...book };
+      const newCart = cart.slice();
+      console.log(newCart.push(b));
+      console.log(newCart);
+      setCart(newCart);
+    }
+  }
+
   return (
     <div>
       <h2>{book.title}</h2>
       <h3>{book.author}</h3>
       <p>{book.price}</p>
       <p>{book.publishedAt}</p>
-      <button>Add to Cart</button>
+      <button onClick={() => addToCart()}>Add to Cart</button>
     </div>
   );
 }
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useLocalStorage } from "../util";
 
 function CartPage() {
-  return <div>Cart items</div>;
+  const [cart, setCart] = useLocalStorage<Array<Book>>("cart", []);
+
+  return (
+    <div>
+      <p>Cart: {cart.length} items</p>
+      {cart.map((book: Book) => (
+        <p>{book.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default CartPage;
